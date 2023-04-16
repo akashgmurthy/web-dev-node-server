73ac07d86a2e57bca50f1aae576a921d9898da17
Akash Ganesamurthy <akashganesamurthy@Akashs-MacBook-Air.local>
2023-04-16 16:03:04 -0400

    events schema and dao

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import EventController from './controllers/events/events-controller.js'
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = 'mongodb+srv://akashgmurthy:ikAYtNs288j2859T@cluster0.t3yfh5o.mongodb.net/rhythm-lounge?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_STRING || 'mongodb://127.0.0.1:27017/rhythm-lounge');
 const app = express()
+
+
+
 app.use(express.json())
 app.use(cors())
 app.get('/', (req, res) => {res.send('Hello World!')})
--- controllers/events/events-controller.js
+++ controllers/events/events-controller.js
@@ -1,10 +1,45 @@
-import events from './events.js'
+import * as eventsDao from './events-dao.js'
+import multer from 'multer';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 
 const EventController = (app) =>{
-    app.get('/events',findEvents)
+    app.get('/events',findEvents);
+    app.get('/events/:eid',findEventsById)
+    app.post('/events',createEvents);
+    app.delete('/events/:eid',deleteEvents)
+    app.put('/events/:eid',updateEvents)
 }
-const findEvents = (req,res) => {
+const findEvents = async (req,res) => {
+    const events = await eventsDao.findEvents()
     res.json(events)
 }
 
+const createEvents = async (req,res) => {
+    const newEvent = req.body;
+    const insertedEvent = await eventsDao.createEvents(newEvent);
+    res.json(insertedEvent)
+}
+
+
+const deleteEvents = async (req,res) => {
+    const eidtodelete = req.params.eid
+    const status = await eventsDao.deleteEvents(eidtodelete)
+    res.json(status)
+}
+
+const findEventsById =  async(req,res) => {
+    const eidtofetch = req.params.eid
+    const status = await eventsDao.findEventsById(eidtofetch)
+    res.json(status)
+}
+
+const updateEvents = async(req,res) => {
+    const eidtoupdate = req.params.eid;
+    const updates =  req.body;
+    const status = eventsDao.updateEvents(eidtoupdate,updates);
+    res.json(status)
+}
 export default EventController;
